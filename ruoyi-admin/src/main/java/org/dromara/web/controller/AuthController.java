8d2644c2b4ea37f41d8d4616f62ed2ea607048b0
疯狂的狮子Li <15040126243@163.com>
2025-06-03 16:13:05 +0800

    reset 修复 satoken异步调用需要手动传递上下文 (跟satoken无关的场景不用处理)

--- ruoyi-admin/src/main/java/org/dromara/web/controller/AuthController.java
+++ ruoyi-admin/src/main/java/org/dromara/web/controller/AuthController.java
@@ -1,7 +1,6 @@
 package org.dromara.web.controller;
 
 import cn.dev33.satoken.annotation.SaIgnore;
-import cn.dev33.satoken.context.mock.SaTokenContextMockUtil;
 import cn.dev33.satoken.exception.NotLoginException;
 import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.core.codec.Base64;
@@ -95,13 +94,10 @@ public class AuthController {
 
         Long userId = LoginHelper.getUserId();
         scheduledExecutorService.schedule(() -> {
-            SaTokenContextMockUtil.setMockContext(() -> {
-                StpUtil.setTokenValueToStorage(loginVo.getAccessToken());
-                SseMessageDto dto = new SseMessageDto();
-                dto.setMessage("欢迎登录RuoYi-Vue-Plus后台管理系统");
-                dto.setUserIds(List.of(userId));
-                SseMessageUtils.publishMessage(dto);
-            });
+            SseMessageDto dto = new SseMessageDto();
+            dto.setMessage("欢迎登录RuoYi-Vue-Plus后台管理系统");
+            dto.setUserIds(List.of(userId));
+            SseMessageUtils.publishMessage(dto);
         }, 5, TimeUnit.SECONDS);
         return R.ok(loginVo);
     }
