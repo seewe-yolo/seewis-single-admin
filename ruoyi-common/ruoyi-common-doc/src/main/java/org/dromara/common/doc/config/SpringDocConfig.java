6bf536b0ca68af120f28d33d74962281341633e7
疯狂的狮子Li <15040126243@163.com>
2025-07-06 10:41:18 +0800

    update 优化 删除无用注释 导致代码报null问题

--- ruoyi-common/ruoyi-common-doc/src/main/java/org/dromara/common/doc/config/SpringDocConfig.java
+++ ruoyi-common/ruoyi-common-doc/src/main/java/org/dromara/common/doc/config/SpringDocConfig.java
@@ -54,14 +54,15 @@ public class SpringDocConfig {
         openApi.externalDocs(properties.getExternalDocs());
         openApi.tags(properties.getTags());
         openApi.paths(properties.getPaths());
-        openApi.components(properties.getComponents());
-        Set<String> keySet = properties.getComponents().getSecuritySchemes().keySet();
-        List<SecurityRequirement> list = new ArrayList<>();
-        SecurityRequirement securityRequirement = new SecurityRequirement();
-        keySet.forEach(securityRequirement::addList);
-        list.add(securityRequirement);
-        openApi.security(list);
-
+        if (properties.getComponents() != null) {
+            openApi.components(properties.getComponents());
+            Set<String> keySet = properties.getComponents().getSecuritySchemes().keySet();
+            List<SecurityRequirement> list = new ArrayList<>();
+            SecurityRequirement securityRequirement = new SecurityRequirement();
+            keySet.forEach(securityRequirement::addList);
+            list.add(securityRequirement);
+            openApi.security(list);
+        }
         return openApi;
     }
 
