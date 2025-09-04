171e6140aebe876e6863260683f9ce67ac201103
AprilWind <2100166581@qq.com>
2025-09-04 15:30:09 +0800

    update 优化全局日期格式转换逻辑

--- ruoyi-common/ruoyi-common-web/src/main/java/org/dromara/common/web/config/ResourcesConfig.java
+++ ruoyi-common/ruoyi-common-web/src/main/java/org/dromara/common/web/config/ResourcesConfig.java
@@ -1,7 +1,8 @@
 package org.dromara.common.web.config;
 
+import cn.hutool.core.date.DateTime;
 import cn.hutool.core.date.DateUtil;
-import org.dromara.common.core.utils.StringUtils;
+import org.dromara.common.core.utils.ObjectUtils;
 import org.dromara.common.web.handler.GlobalExceptionHandler;
 import org.dromara.common.web.interceptor.PlusWebInvokeTimeInterceptor;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
@@ -34,10 +35,11 @@ public class ResourcesConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
         // 全局日期格式转换配置
         registry.addConverter(String.class, Date.class, source -> {
-            if (StringUtils.isBlank(source)) {
+            DateTime parse = DateUtil.parse(source);
+            if (ObjectUtils.isNull(parse)) {
                 return null;
             }
-            return DateUtil.parse(source);
+            return parse.toJdkDate();
         });
     }
 
