7df3909112cab81511b11bdbba328ac45194be33
疯狂的狮子Li <15040126243@163.com>
2025-12-10 17:01:19 +0800

    update warm-flow 1.8.3 => 1.8.4

--- ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/domain/vo/FlowTaskVo.java
+++ ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/domain/vo/FlowTaskVo.java
@@ -158,7 +158,7 @@ public class FlowTaskVo implements Serializable {
     /**
      * 流程签署比例值 大于0为票签，会签
      */
-    private BigDecimal nodeRatio;
+    private String nodeRatio;
 
     /**
      * 申请人id
--- ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/service/impl/FlwTaskServiceImpl.java
+++ ruoyi-modules/ruoyi-workflow/src/main/java/org/dromara/workflow/service/impl/FlwTaskServiceImpl.java
@@ -31,6 +31,7 @@ import org.dromara.common.satoken.utils.LoginHelper;
 import org.dromara.warm.flow.core.FlowEngine;
 import org.dromara.warm.flow.core.dto.FlowParams;
 import org.dromara.warm.flow.core.entity.*;
+import org.dromara.warm.flow.core.enums.CooperateType;
 import org.dromara.warm.flow.core.enums.NodeType;
 import org.dromara.warm.flow.core.enums.SkipType;
 import org.dromara.warm.flow.core.enums.UserType;
@@ -62,7 +63,6 @@ import org.dromara.workflow.service.IFlwTaskService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.math.BigDecimal;
 import java.util.*;
 
 import static org.dromara.workflow.common.constant.FlowConstant.*;
@@ -744,7 +744,7 @@ public class FlwTaskServiceImpl implements IFlwTaskService {
         Task task = taskService.getById(taskId);
         FlowNode flowNode = getByNodeCode(task.getNodeCode(), task.getDefinitionId());
         if (ADD_SIGNATURE.equals(taskOperation) || REDUCTION_SIGNATURE.equals(taskOperation)) {
-            if (flowNode.getNodeRatio().compareTo(BigDecimal.ZERO) == 0) {
+            if (!CooperateType.isCountersign(flowNode.getNodeRatio())) {
                 throw new ServiceException(task.getNodeName() + "不是会签节点！");
             }
         }
