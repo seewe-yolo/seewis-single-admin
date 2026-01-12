ed01f044cf77c1c393cf35bd51ab8bc0a3b7e0a3
疯狂的狮子Li <15040126243@163.com>
2026-01-12 09:17:39 +0800

    update 优化 大家都认可用"账"统一改为账

--- ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/domain/SysUser.java
+++ ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/domain/SysUser.java
@@ -78,7 +78,7 @@ public class SysUser extends BaseEntity {
     private String password;
 
     /**
-     * 帐号状态（0正常 1停用）
+     * 账号状态（0正常 1停用）
      */
     private String status;
 
--- ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/domain/vo/SysUserVo.java
+++ ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/domain/vo/SysUserVo.java
@@ -84,7 +84,7 @@ public class SysUserVo implements Serializable {
     private String password;
 
     /**
-     * 帐号状态（0正常 1停用）
+     * 账号状态（0正常 1停用）
      */
     private String status;
 
