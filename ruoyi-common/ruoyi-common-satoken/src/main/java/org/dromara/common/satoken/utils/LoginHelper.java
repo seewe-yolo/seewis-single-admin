2113e690be2c621d2bfeab4437979bc1d3faeb95
疯狂的狮子Li <15040126243@163.com>
2026-03-02 13:27:53 +0800

    update 优化 消除编译相关提醒

--- ruoyi-common/ruoyi-common-satoken/src/main/java/org/dromara/common/satoken/utils/LoginHelper.java
+++ ruoyi-common/ruoyi-common-satoken/src/main/java/org/dromara/common/satoken/utils/LoginHelper.java
@@ -58,7 +58,7 @@ public class LoginHelper {
     /**
      * 获取用户(多级缓存)
      */
-    @SuppressWarnings("unchecked cast")
+    @SuppressWarnings("unchecked")
     public static <T extends LoginUser> T getLoginUser() {
         SaSession session = StpUtil.getTokenSession();
         if (ObjectUtil.isNull(session)) {
@@ -70,7 +70,7 @@ public class LoginHelper {
     /**
      * 获取用户基于token
      */
-    @SuppressWarnings("unchecked cast")
+    @SuppressWarnings("unchecked")
     public static <T extends LoginUser> T getLoginUser(String token) {
         SaSession session = StpUtil.getTokenSessionByToken(token);
         if (ObjectUtil.isNull(session)) {
